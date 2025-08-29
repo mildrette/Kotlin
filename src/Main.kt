88d7befa876cee7edd89db26645f0e12c093102a
mildrette <mildredfonka1@maild.com>
2025-08-29 11:44:19 +0100

    my first kotlin code

---
+++ src/Main.kt
@@ -0,0 +1,13 @@
+ fun main(){
+    val name = "mildred"
+     val skills = mutableListOf("java", "javascript")
+     val skill = listOf("html", "css", "react")
+
+     skills.add("kotlin")
+     skills.remove("java")
+     skills[1] = "reatJs"
+
+println(skills)
+     println(skill)
+    println(name)
+}
