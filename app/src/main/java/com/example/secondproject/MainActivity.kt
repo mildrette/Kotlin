f8ee4cfe3ab678ad6610e1eb924088bf05b0675f
mildrette <mildredfonka1@maild.com>
2025-09-18 14:41:09 +0100

    use compose to display the kotlin variables, loops and conditionals i have learned

--- app/src/main/java/com/example/secondproject/MainActivity.kt
+++ app/src/main/java/com/example/secondproject/MainActivity.kt
@@ -3,45 +3,71 @@ package com.example.secondproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.secondproject.ui.theme.SecondProjectTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             SecondProjectTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                MiniAgeCheckerLayout()
                 }
             }
         }
     }
-}
+
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MiniAgeCheckerLayout(){
+    val names = arrayOf("Mildred", "favour", "Paula", "Maureen")
+    val ages = arrayOf(70, 23, 5, 18)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SecondProjectTheme {
-        Greeting("Android")
+    Column( modifier = Modifier
+        .fillMaxSize()
+        .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)){
+        for ( i in names.indices){
+            val name = names[i]
+            val age = ages[i]
+
+            Box( modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+            ){
+                Column {
+                    Text(text = "Name: $name", fontSize = 30.sp)
+                    Text(text = "Aga: $age", fontSize= 25.sp)
+
+
+                    if (age < 18) {
+                        Text(text = "$name is a child")
+                    } else if (age in 18..60) {
+                        Text(text = "$name is an adult")
+                    } else {
+                        Text(text = "$name is a senior")
+                    }
+                }
+            }
+        }
     }
+    
+}
+
+
+@Preview
+@Composable
+fun Preview(){
+    
 }
